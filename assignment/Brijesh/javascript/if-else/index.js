09b970f3a7d748807f1481f3fc2575ebd68de486
BRIJESH PRAJAPATI <userbrijesh@gmail.com>
2022-01-31 19:34:17 +0530

    javascript function loop

---
+++ assignment/Brijesh/javascript/if-else/index.js
@@ -0,0 +1,75 @@
+
+
+// let a = 25;
+// let b = 42;
+// let result = a+b;
+// console.log(result);
+
+// let b = 765;
+// let a =785;
+// var c = a+b;
+// console.log(c);
+
+
+
+
+
+// check even or odd;
+
+// let num = 321374;
+
+// if( num % 2 == 0){
+//     console.log("The given number is even");
+// }else{
+//     console.log("The given number is odd");
+// }
+
+
+
+
+// check whether a number is positive , neagative, odd;
+
+// let num = 0;
+
+// if (num > 0){
+//     console.log('the given number is positive.');
+// }else if(num == 0 ){
+//     console.log('the given number is zero.');
+// }else{
+//     console.log('the given number is negative.')
+// }
+
+
+// compare three numbers
+
+// let num1 = 12.5;
+// let num2 = 40.6;
+// let num3 = 41.2;
+
+// if(num1 >= num2 && num1 >= num3){
+//     console.log("the greatest num is :" + num1 );
+
+// }else if(num2 >= num1 && num2 >= num3){
+//     console.log("the greatest num is :" + num2 );
+
+// }else{
+//     console.log('the greatest number is:' + num3);
+// }
+
+
+
+// factorial
+
+
+//  let num = 25;
+// if(num < 0){
+//     console.log("factorial of negative number does't exist. ")
+// }else if(num == 0){
+//     console.log("factorial of zero is 1");
+// }else{
+//     let fact = 1;
+//     for(i = 1; i<= num; i++){
+//         fact *= i;
+//     }
+//     console.log('the factorial of ' + num + ' is ' + fact);
+// }
